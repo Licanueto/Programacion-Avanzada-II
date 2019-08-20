510cb449ce239ccdea666ac3cf3f8bcbabe212ac
Fasuto Moya <faustomoya.99@hotmail.com>
2019-08-19 23:36:38 -0300

    19/8

---
+++ TP-1/2/script.js
@@ -0,0 +1,53 @@
+var items = [{info:"item 1",id: 1},
+               {info:"item 2",id: 2},
+               {info:"item 3",id: 3}];
+
+var agregados = []
+
+function loadInfo(){
+    items.forEach((item)=>{
+        document.getElementById("lista").innerHTML = document.getElementById("lista").innerHTML + '<div class="item"><div class="right floated content"><div onClick="addItem('+item.id+')" class="ui button">Add</div></div><div class="content">'+item.info+'</div></div>'
+
+    })
+    var body = document.getElementById("body")
+    body.removeChild(document.getElementById("boton"))
+}
+
+function itemIsAdded(id){
+    var rta = false;
+    agregados.forEach(item => {
+        if(item.id === id){
+            rta = true;
+        }
+    })
+    return rta;
+}
+
+function printItems(){
+    document.getElementById("agregados").innerHTML = ""
+    agregados.forEach((item)=>{
+        document.getElementById("agregados").innerHTML = document.getElementById("agregados").innerHTML + '<div class="item"><div class="right floated content"><div onClick="removeItem('+item.id+')" class="ui button">Remove</div></div><div class="content">'+item.info+'</div></div>'
+    }) 
+}
+
+function addItem(id){
+    if(agregados.length > 0){
+        if(!itemIsAdded(id)){
+             agregados.push(items.find((item)=> item.id === id))
+             printItems();
+        }
+        else{
+            alert("item ya agregado");
+        }
+    }else{
+        agregados.push(items.find((item)=> item.id === id))
+        printItems();
+    }
+}
+
+function removeItem(id){
+    agregados = agregados.filter(item => item.id !== id)
+    printItems();
+}
+    
+
