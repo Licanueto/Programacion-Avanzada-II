b9127d5e164b9b4bc62b47e465d42de635d63748
Fasuto Moya <faustomoya.99@hotmail.com>
2019-10-15 20:41:09 -0300

    15/10

--- TP-Angular/src/app/components/student-add/student-add.component.ts
+++ TP-Angular/src/app/components/student-add/student-add.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Student } from 'src/app/models/student';
 import { StudentService } from 'src/app/services/student.service'
 import { ActivatedRoute } from '@angular/router';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 
 @Component({
@@ -10,27 +11,24 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./student-add.component.css']
 })
 export class StudentAddComponent implements OnInit {
+  studentForm: FormGroup;
+  student: Student = new Student();
 
-  name: String;
-  lastName: String;
-  dni: String;
-  email: String;
-  adress: String;
 
   constructor(private studentService: StudentService, private route: ActivatedRoute) { }
 
   ngOnInit() {
+   this.studentForm = new FormGroup({
+      'firstName': new FormControl(this.student.firstName,[Validators.required]),
+      'lastName': new FormControl(this.student.lastName,[Validators.required]),
+      'email': new FormControl(this.student.email,[Validators.required]),
+      'dni': new FormControl(this.student.dni,[Validators.required]),
+      'address': new FormControl(this.student.address,[Validators.required])
+    })
   }
 
   addStudent(){
-    let student = new Student();
-    student.address = this.adress;
-    student.dni = this.dni;
-    student.email = this.email;
-    student.firstName = this.name;
-    student.lastName = this.lastName;
-    console.log("hola")
-    this.studentService.addStudent(student)
+    this.studentService.addStudent(this.student)
     .then(() => alert("ok"))
     .catch(() => console.log("error"))
   }
