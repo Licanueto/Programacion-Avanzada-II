07e7db9d700ca691648bd16cf0ebd713a2f14501
Fasuto Moya <faustomoya.99@hotmail.com>
2019-10-16 23:52:08 -0300

    16/10

--- TP-Angular/src/app/components/student-add/student-add.component.ts
+++ TP-Angular/src/app/components/student-add/student-add.component.ts
@@ -15,22 +15,22 @@ export class StudentAddComponent implements OnInit {
   student: Student = new Student();
 
 
-  constructor(private studentService: StudentService, private route: ActivatedRoute) { }
+  constructor(private studentService: StudentService, private route: ActivatedRoute) { 
+  }
 
   ngOnInit() {
    this.studentForm = new FormGroup({
-      'firstName': new FormControl(this.student.firstName,[Validators.required]),
-      'lastName': new FormControl(this.student.lastName,[Validators.required]),
-      'email': new FormControl(this.student.email,[Validators.required]),
-      'dni': new FormControl(this.student.dni,[Validators.required]),
-      'address': new FormControl(this.student.address,[Validators.required])
-    })
+      'firstName': new FormControl(this.student.firstName),
+      'lastName': new FormControl(this.student.lastName),
+      'email': new FormControl(this.student.email),
+      'dni': new FormControl(this.student.dni),
+      'address': new FormControl(this.student.address)
+    })    
   }
 
   addStudent(){
-    this.studentService.addStudent(this.student)
+    this.studentService.addStudent(this.studentForm.value)
     .then(() => alert("ok"))
     .catch(() => console.log("error"))
   }
-
 }
